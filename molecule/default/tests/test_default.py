e0abc5b3415bb20da42b5b98c2de9b946e30d1bf
Hadrien Patte <hadrien.patte@protonmail.com>
2020-02-11 03:05:52 +0100

    Configure SSH
    Signed-off-by: Hadrien Patte <hadrien.patte@protonmail.com>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,12 +2,48 @@ import os
 
 import testinfra.utils.ansible_runner
 
+import pytest
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+@pytest.mark.parametrize('name', [
+    ('openssh-server'),
+])
+def test_package_is_installed(host, name):
+    package = host.package(name)
+    assert package.is_installed
+
+
+@pytest.mark.parametrize('name', [
+    ('ssh'),
+])
+def test_service_is_running(host, name):
+    service = host.service(name)
+    assert service.is_running
+
+
+@pytest.mark.parametrize('name', [
+    ('ssh'),
+])
+def test_service_is_enabled(host, name):
+    service = host.service(name)
+    assert service.is_enabled
+
+
+@pytest.mark.parametrize('port', [
+    ('22'),
+])
+def test_socket(host, port):
+    assert host.socket('tcp://0.0.0.0:' + port).is_listening
+
+
+@pytest.mark.parametrize('line', [
+    ('PermitRootLogin no'),
+    ('PasswordAuthentication no'),
+])
+def test_ssh_config(host, line):
+    file = host.file('/etc/ssh/sshd_config')
+    assert file.exists
+    assert file.contains(line)
